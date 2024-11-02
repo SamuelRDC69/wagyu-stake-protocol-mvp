953b92a412bcf49b585d2997167e87817a4fed97
SamuelRDC69 <79984988+SamuelRDC69@users.noreply.github.com>
2024-11-02 23:44:43 +0000

    Update index.tsx

---
+++ src/components/common/index.tsx
@@ -0,0 +1,12 @@
+// src/components/common/index.ts
+export { default as Button } from './Button';
+export {
+  Typography,
+  Card,
+  Badge,
+  AnimatedNumber,
+  LoadingSpinner,
+  Progress,
+  Tooltip,
+  IconButton
+} from './components';
