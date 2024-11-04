9eae705d92380c8eb916ab6d742c9df2d9e77b6b
SamuelRDC69 <79984988+SamuelRDC69@users.noreply.github.com>
2024-11-04 18:38:11 +0000

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,31 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+// src/main.tsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+
+// Context Providers
+import { NotificationsProvider } from './contexts/NotificationsContext';
+import { SettingsProvider } from './contexts/SettingsContext';
+import { PoolProvider } from './contexts/PoolContext';
+import { GuildProvider } from './contexts/GuildContext';
+import { ChallengesProvider } from './contexts/ChallengesContext';
+import { AnalyticsProvider } from './contexts/AnalyticsContext';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <NotificationsProvider>
+      <SettingsProvider>
+        <PoolProvider>
+          <GuildProvider>
+            <ChallengesProvider>
+              <AnalyticsProvider>
+                <App />
+              </AnalyticsProvider>
+            </ChallengesProvider>
+          </GuildProvider>
+        </PoolProvider>
+      </SettingsProvider>
+    </NotificationsProvider>
   </React.StrictMode>
-)
+);
