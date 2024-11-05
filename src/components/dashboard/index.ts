0e561c2e4cf358d8a55735d51660dc524470abd9
SamuelRDC69 <79984988+SamuelRDC69@users.noreply.github.com>
2024-11-05 23:50:23 +0000

    Update index.ts

--- src/components/dashboard/index.ts
+++ src/components/dashboard/index.ts
@@ -1,17 +1,6 @@
-// src/components/Dashboard/index.ts
-import ClaimProjectionGauge from './ClaimProjectionGauge';
-import CooldownTimer from './CooldownTimer';
-import RiskMeter from './RiskMeter';
-import TierBadge from './TierBadge';
-import StakingStats from './StakingStats';
-import DashboardView from './DashboardView';
-
-export {
-  ClaimProjectionGauge,
-  CooldownTimer,
-  RiskMeter,
-  TierBadge,
-  StakingStats
-};
-
-export default DashboardView;
+export { default as ClaimProjectionGauge } from './ClaimProjectionGauge';
+export { default as CooldownTimer } from './CooldownTimer';
+export { default as RiskMeter } from './RiskMeter';
+export { default as TierBadge } from './TierBadge';
+export { default as StakingStats } from './StakingStats';
+export { default } from './DashboardView';
