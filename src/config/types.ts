2a3a2e02a1b4c9be8f14517df6ef9c661e18a563
SamuelRDC69 <79984988+SamuelRDC69@users.noreply.github.com>
2024-11-12 16:40:06 +0000

    Update types.ts

--- src/config/types.ts
+++ src/config/types.ts
@@ -1,18 +1,18 @@
-// src/config/types.ts
 export interface ConfigData {
   maintenance: boolean;
   cooldown_seconds_per_claim: number;
   vault_account: string;
 }
 
-export interface TierData {
+export interface TierEntity {
   tier: string;
   tier_name: string;
   weight: number;
   staked_up_to_percent: number;
 }
 
-export interface PoolData {
+export interface PoolEntity {
+  pool_id: number;
   staked_token_contract: string;
   staked_token_symbol: string;
   total_staked_quantity: string;
@@ -24,13 +24,9 @@ export interface PoolData {
   emission_unit: number;
   emission_rate: number;
   last_emission_updated_at: string;
-}
-
-export interface TierEntity extends TierData {
-  id?: number;
-}
-
-export interface PoolEntity extends PoolData {
-  pool_id: number;
   is_active: boolean;
 }
+
+// Types for creating new entities
+export type NewPoolData = Omit<PoolEntity, 'pool_id' | 'is_active'>;
+export type NewTierData = Omit<TierEntity, 'id'>;
