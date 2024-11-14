9cce8d8078c38eac5da46df175f07639c9ac23bb
SamuelRDC69 <79984988+SamuelRDC69@users.noreply.github.com>
2024-11-14 03:17:28 +0000

    Update BottomNav.tsx

--- src/components/layout/BottomNav.tsx
+++ src/components/layout/BottomNav.tsx
@@ -1,4 +1,4 @@
-import { Group, UnstyledButton, Stack, Text } from '@mantine/core';
+import { Group, UnstyledButton, Stack, Text, Footer } from '@mantine/core';
 import { IconChartBar, IconCoin, IconHistory } from '@tabler/icons-react';
 import { useLocation, useNavigate } from 'react-router-dom';
 
@@ -13,31 +13,34 @@ export function BottomNav() {
   const navigate = useNavigate();
 
   return (
-    <Group 
-      grow 
-      h="100%" 
-      px="md"
-      justify="center"
-    >
-      {navItems.map((item) => (
-        <UnstyledButton
-          key={item.path}
-          onClick={() => navigate(item.path)}
-          sx={(theme) => ({
-            color: location.pathname === item.path 
-              ? theme.colors.orange[4] 
-              : theme.colors.gray[6],
-            '&:hover': {
-              color: theme.colors.orange[4]
-            }
-          })}
-        >
-          <Stack align="center" spacing={4}>
-            <item.icon size={24} />
-            <Text size="xs">{item.label}</Text>
-          </Stack>
-        </UnstyledButton>
-      ))}
-    </Group>
+    <Footer height={60}>
+      <Group 
+        sx={{ height: '100%' }} 
+        position="apart" 
+        px="md"
+        spacing={0}
+        grow
+      >
+        {navItems.map((item) => (
+          <UnstyledButton
+            key={item.path}
+            onClick={() => navigate(item.path)}
+            sx={(theme) => ({
+              color: location.pathname === item.path 
+                ? theme.colors.orange[4] 
+                : theme.colors.gray[6],
+              '&:hover': {
+                color: theme.colors.orange[4]
+              }
+            })}
+          >
+            <Stack align="center" spacing={4}>
+              <item.icon size={24} />
+              <Text size="xs">{item.label}</Text>
+            </Stack>
+          </UnstyledButton>
+        ))}
+      </Group>
+    </Footer>
   );
 }
