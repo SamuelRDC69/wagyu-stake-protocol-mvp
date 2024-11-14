fcfa9af7ae0685682f5623fb7181f5aa1a944202
SamuelRDC69 <79984988+SamuelRDC69@users.noreply.github.com>
2024-11-14 03:05:47 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,27 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      include: "**/*.tsx",
+    })
+  ],
+  css: {
+    postcss: {
+      plugins: [
+        require('postcss-preset-mantine'),
+        require('postcss-simple-vars')({
+          variables: {
+            'mantine-breakpoint-xs': '36em',
+            'mantine-breakpoint-sm': '48em',
+            'mantine-breakpoint-md': '62em',
+            'mantine-breakpoint-lg': '75em',
+            'mantine-breakpoint-xl': '88em',
+          },
+        }),
+      ],
+    },
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
