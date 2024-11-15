ae665a9d9488dd022b4d8b27f2ac02fb412c610a
SamuelRDC69 <79984988+SamuelRDC69@users.noreply.github.com>
2024-11-15 17:21:54 +0000

    Update context.ts

--- src/lib/wharfkit/context.ts
+++ src/lib/wharfkit/context.ts
@@ -1,10 +1,12 @@
 import { createContext } from 'react';
-import { Session } from '@wharfkit/session';
+import { Session, SessionKit } from '@wharfkit/session';
 
 export const WharfkitContext = createContext<{
   session: Session | undefined;
   setSession: (session: Session | undefined) => void;
+  sessionKit: SessionKit;
 }>({
   session: undefined,
   setSession: () => {},
+  sessionKit: {} as SessionKit,
 });
