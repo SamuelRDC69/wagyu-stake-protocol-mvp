944d9ce27f43cdc463c19c86c2f04c4220288282
SamuelRDC69 <79984988+SamuelRDC69@users.noreply.github.com>
2024-11-16 00:09:00 +0000

    Update contracts.ts

---
+++ src/lib/wharfkit/contracts.ts
@@ -0,0 +1,9 @@
+export const CONTRACTS = {
+  STAKING: {
+    NAME: 'kekkekkekkek', // Replace with your deployed contract name
+    TABLES: {
+      POOLS: 'pools',
+      STAKEDS: 'stakeds'
+    }
+  }
+} as const;
