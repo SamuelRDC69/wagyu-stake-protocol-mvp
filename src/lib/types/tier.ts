158916c8d178505a59b46f0bd03b0ad4821231dd
SamuelRDC69 <79984988+SamuelRDC69@users.noreply.github.com>
2024-11-17 22:18:06 +0000

    Update tier.ts

--- src/lib/types/tier.ts
+++ src/lib/types/tier.ts
@@ -1,8 +1,8 @@
 export interface TierEntity {
   tier: string;               // "bronze" | "silver" | "gold"
   tier_name: string;          // "Bronze Tier" | "Silver Tier" | "Gold Tier"
-  weight: number;             // 2.0, 4.0
-  staked_up_to_percent: number; // 1.0, 5.0, 100.0
+  weight: string;             // 2.0, 4.0
+  staked_up_to_percent: string; // 1.0, 5.0, 100.0
 }
 
 export interface TierProgress {
