5e8b0dda902be623479ef3b912f0c3ba26d30f8f
SamuelRDC69 <79984988+SamuelRDC69@users.noreply.github.com>
2024-11-18 23:42:32 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Session, SessionKit, Chains } from '@wharfkit/session'
 import { WalletPluginAnchor } from '@wharfkit/wallet-plugin-anchor'
 import WebRenderer from '@wharfkit/web-renderer'
 import GameUI from './components/GameUI'
+import { NotificationContainer } from './components/NotificationContainer'
 import { WharfkitContext } from './lib/wharfkit/context'
 import './index.css'  // For Tailwind and shadcn/ui styles
 import './App.css'    // For your custom styles
@@ -20,11 +21,12 @@ function App() {
   const [session, setSession] = useState<Session | undefined>()
 
   return (
-  <WharfkitContext.Provider value={{ session, setSession, sessionKit }}>
-    <div className="App">
-      <GameUI />
-    </div>
-  </WharfkitContext.Provider>
+    <WharfkitContext.Provider value={{ session, setSession, sessionKit }}>
+      <div className="App">
+        <GameUI />
+        <NotificationContainer />
+      </div>
+    </WharfkitContext.Provider>
   )
 }
 
