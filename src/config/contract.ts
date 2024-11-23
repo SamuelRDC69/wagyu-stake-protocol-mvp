81258118daa408940616d981bee1a3f2db136d03
SamuelRDC69 <79984988+SamuelRDC69@users.noreply.github.com>
2024-11-23 01:28:48 +0000

    Update contract.ts

--- src/config/contract.ts
+++ src/config/contract.ts
@@ -3,7 +3,7 @@ import { WalletPluginAnchor } from "@wharfkit/wallet-plugin-anchor"
 import WebRenderer from "@wharfkit/web-renderer"
 import { Chains } from "@wharfkit/session"
 
-export const CONTRACT_ACCOUNT = "stakingdappp" // Replace with your contract account
+export const CONTRACT_ACCOUNT = "testnextproj" // Replace with your contract account
 
 export const sessionKit = new SessionKit({
   appName: "Stakeland Admin",
