94affad5e5f875558a47b057cfc4d68b1b48676b
Anon-on-chain <sammchambertwo@gmail.com>
2024-11-24 21:55:20 +0000

    Update animation.ts

--- src/lib/hooks/animation.ts
+++ src/lib/hooks/animation.ts
@@ -1,11 +1,10 @@
+// src/lib/hooks/animation.ts
 import { useRef, useEffect } from "react";
 
 export function usePrevious<T>(value: T): T | undefined {
   const ref = useRef<T>();
-
   useEffect(() => {
     ref.current = value;
   });
-
   return ref.current;
 }
