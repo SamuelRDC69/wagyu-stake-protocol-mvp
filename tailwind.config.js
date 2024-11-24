2db18c61486cf0812b93203079e92ec287f814b9
Anon-on-chain <sammchambertwo@gmail.com>
2024-11-24 17:26:47 +0000

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
-/** @type {import('tailwindcss').Config} */
+/ @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    './pages//.{ts,tsx}',
+    './components/**/.{ts,tsx}',
+    './app//*.{ts,tsx}',
+    './src//*.{ts,tsx}',
   ],
   theme: {
     container: {
@@ -65,11 +65,19 @@ module.exports = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: 0 },
         },
+      },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out"
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
   plugins: [],
+}
+Post
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 }
