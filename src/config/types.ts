41a420e747d4af70a7e78fbeb90bce31095f43d0
SamuelRDC69 <79984988+SamuelRDC69@users.noreply.github.com>
2024-12-18 22:22:11 +0000

    Update types.ts

--- src/config/types.ts
+++ src/config/types.ts
@@ -2,6 +2,8 @@ export interface ConfigData {
   maintenance: boolean;
   cooldown_seconds_per_claim: number;
   vault_account: string;
+  fee_vault_account: string;      // New field
+  stake_fee_basis_points: number; // New field
 }
 
 export interface TierEntity {
@@ -23,6 +25,8 @@ export interface PoolEntity {
   };
   emission_unit: number;
   emission_rate: number;
+  emission_start_at: string;    // New field
+  emission_end_at: string;      // New field
   last_emission_updated_at: string;
   is_active: boolean;
 }
