06838acb5cf7123ff17f4bec0c19560a9428abcf
Anon-on-chain <sammchambertwo@gmail.com>
2025-02-18 03:59:16 +0000

    Update GameUI.tsx

--- src/components/GameUI.tsx
+++ src/components/GameUI.tsx
@@ -476,7 +476,7 @@ const userStatusProps = useMemo(() => ({
 const BerryFiLogo = () => {
   return (
     <h1 className="flex items-baseline">
-      <span className="font-imaki text-3xl bg-gradient-to-r from-purple-200 to-purple-300 text-transparent bg-clip-text">
+      <span className="font-berryfield text-3xl bg-gradient-to-r from-purple-200 to-purple-300 text-transparent bg-clip-text">
         Berry
       </span>
       <span className="font-heading text-3xl bg-gradient-to-r from-purple-300 to-purple-400 text-transparent bg-clip-text">
